8a4463e321060584f36ec703ff743400fb6911ec
bloodtypebpos <matt.tigrett@gmail.com>
2015-09-29 12:04:35 -0400

    Update BookTest.java

--- Book/BookTest.java
+++ Book/BookTest.java
@@ -5,12 +5,16 @@ public class BookTest
 {
    public static void main(String[] args)
    {
-      Book book1 = new Book("Herman Melville", "Mobby Dick", 34.56);
+      Book book1 = new Book("Cay Horstmann", "Java Concepts", 99.99, 20);
       System.out.println("The author of book1 is " + book1.getAuthor());
       System.out.println("The title of book1 is " + book1.getTitle());
       System.out.println("The price of book1 is " + book1.getPrice());
+
+      book1.setAuthor("Herman Melville");
+      book1.setTitle("Moby Dick");
+      book1.setPrice(100);
       System.out.println();
-      book1.discountPrice(0.10);
+      book1.discountPrice(20);
       book1.displayBook();
 
 
