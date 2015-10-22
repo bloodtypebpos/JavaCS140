3ea79d6363048a40ef154a491117dcf4818d1dd0
bloodtypebpos <matt.tigrett@gmail.com>
2015-10-21 20:21:48 -0400

    Update GradeTest.java

--- Exercises/Grade/GradeTest.java
+++ Exercises/Grade/GradeTest.java
@@ -18,10 +18,17 @@ public class GradeTest
 		 System.out.println("Please provide a grade in the form of an integer from 0 to 100");
 		 input = console.readLine();
 		 grade = Integer.parseInt(input);
+		 if(grade < 0){
+			 System.out.println("That is an invalid entry. Exiting the program");
+			 System.exit(0);
+		 }else if(grade > 100){
+			 System.out.println("That is an invalid entry. Exiting the program");
+			 System.exit(0);
+		 }else{
 		 Grade myGrade = new Grade(grade);
 		 System.out.println("The letter grade for the provided number grade is:");
 		 System.out.println(myGrade.getLetterGrade());
-
+	 	 }
 	     //Add System.exit(0); - correct end of the program
          System.exit(0);
 	 }
