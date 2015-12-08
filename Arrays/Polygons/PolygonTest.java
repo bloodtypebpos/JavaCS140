07e035950ffda790c6c64060b8ec0b24260b5156
bloodtypebpos <matt.tigrett@gmail.com>
2015-12-08 13:30:43 -0500

    Create PolygonTest.java

---
+++ Arrays/Polygons/PolygonTest.java
@@ -0,0 +1,15 @@
+
+public class PolygonTest
+{
+
+   public static void main(String[] args)
+   {
+      Polygon test = new Polygon(5);
+
+      test.setPoints();
+
+      test.getArea();
+
+
+   }
+}
