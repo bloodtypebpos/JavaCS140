4be0aa52b0d78314dc392d4541437e2581a31a87
bloodtypebpos <matt.tigrett@gmail.com>
2015-12-10 12:43:30 -0500

    Create XYpointsTest.java

---
+++ Arrays/XYpoints/XYpointsTest.java
@@ -0,0 +1,14 @@
+public class XYpointsTest
+{
+
+   public static void main(String[] args)
+   {
+      XYpoints test = new XYpoints();
+
+      test.setPoints();
+
+      test.getThePoints();
+
+
+   }
+}
